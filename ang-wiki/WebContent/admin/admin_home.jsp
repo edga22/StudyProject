3804a667af2f215ceea4477df154fb89b0bb0337
angel <angel@Angel-Book>
2017-02-13 12:43:17 +0900

    어드민 페이지 기본작업
    서블릿 URL 필터링 적용함

--- ang-wiki/WebContent/admin/admin_home.jsp
+++ ang-wiki/WebContent/admin/admin_home.jsp
@@ -1,10 +1,13 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%
+
+%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<title>어드민 페이지입니다</title>
 </head>
 <body>
 
---
+++ ang-wiki/src/system/Admin.java
@@ -0,0 +1,45 @@
+package system;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Admin
+ */
+@WebServlet("/admin")
+public class Admin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Admin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		String memID = "";
+		if(session.getAttribute("memID")!=null)memID = (String)session.getAttribute("memID");
+		
+		if(memID.equals("admin")) {					
+			RequestDispatcher view = request.getRequestDispatcher("/admin/admin_home.jsp");
+			view.forward(request, response);
+		}
+		else{
+			response.sendRedirect("/index.jsp");
+		}
+	}
+}
